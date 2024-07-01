856786c972445909086fd2982cc09ba27c10d200
Frederico Pena Santiago <frede.stg@gmail.com>
2024-07-01 12:10:15 -0300

    feat: How it works section

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./components/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        geist: ["Geist", "sans-serif"],
+        "geist-mono": ["Geist Mono", "monospace"],
+      },
+      colors: {
+        brown: "#6B2E2E",
+        "soft-brown": "#fcc",
+        green: "3CDD8C",
+        "soft-green": "D4F7E6",
+        yellow: "#FFC435",
+        "soft-yellow": "#fec",
+        grey: "#F3F3F3",
+        "soft-grey": "#fff",
+        pink: "#E699D9",
+        "soft-pink": "#FFCCF7",
+        purple: "#3A2234",
+        "soft-purple": "#EDDEE9",
+        blue: "#7575F0",
+        "soft-blue": "#E8E8FC",
+      },
+    },
+  },
+};
