612246b1ce57affca2ccf808cd03c4e974f873d8
Frederico Pena Santiago <frede.stg@gmail.com>
2024-07-01 15:56:07 -0300

    fix: Organize repo to enable vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./components/*.html"],
+  content: ["./src/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
