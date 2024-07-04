f2a6cfe18f47be253f93319ac6636a33d25a8a3c
Frederico Pena Santiago <frede.stg@gmail.com>
2024-07-04 19:04:23 -0300

    feat: Component preview animations

--- js/copyCode.js
+++ js/copyCode.js
@@ -1,21 +1,33 @@
 document.querySelectorAll(".copy-button").forEach((button) => {
   button.addEventListener("click", async () => {
     const codeContainer = button.closest(".code-container");
-    const buttonContent = button.querySelector(".button-content");
+    // const buttonContent = button.querySelector(".button-content");
     const copiedText = button.querySelector(".copied-text");
     const codeSnippet = codeContainer.querySelector(".code-snippet").innerText;
 
-    buttonContent.classList.add("hidden");
-    copiedText.classList.remove("hidden");
+    copiedText.parentElement.classList.add("rounded-r-[0.5rem]");
+    copiedText.parentElement.classList.remove("rounded-[0.5rem]");
+    copiedText.classList.add("w-fit");
+    copiedText.classList.add("px-1");
+    copiedText.classList.remove("w-0");
+
+    copiedText.style.transform = "translateX(-2rem)";
 
     try {
       button.disabled = true;
       await navigator.clipboard.writeText(codeSnippet);
 
       setTimeout(() => {
-        buttonContent.classList.remove("hidden");
-        copiedText.classList.add("hidden");
-        button.disabled = false;
+        copiedText.style.transform = "translateX(0rem)";
+
+        copiedText.addEventListener("transitionend", () => {
+          copiedText.classList.add("w-0");
+          copiedText.classList.remove("w-fit");
+          copiedText.classList.remove("px-1");
+          copiedText.parentElement.classList.add("rounded-[0.5rem]");
+          copiedText.parentElement.classList.remove("rounded-r-[0.5rem]");
+          button.disabled = false;
+        });
       }, 2000);
     } catch {
       console.error("Failed to copy the code to the clipboard");
--- js/togglePreview.js
+++ js/togglePreview.js
@@ -1,30 +1,35 @@
-document.querySelectorAll(".toggle-button").forEach((button) => {
+const tabButtons = document.querySelectorAll(".toggle-button");
+const codeContainers = document.querySelectorAll(".code-container");
+
+const container = document.querySelector(".button-container");
+tabButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    // Hide all elements
-    document.querySelectorAll(".toggle-element").forEach((element) => {
-      element.classList.add("hidden");
-      element.classList.remove("flex");
+    const highlighter = document.getElementById("highlighter");
+    // Remove 'active' class from all button containers
+    codeContainers.forEach((container) => {
+      container.style.display = "none";
+    });
+    tabButtons.forEach((btn) => {
+      btn.parentElement.classList.remove("active");
+      btn.parentElement.classList.add("text-white/80");
+      btn.parentElement.classList.add("hover:text-white");
+      btn.parentElement.classList.remove("text-purple");
     });
 
-    // Show the target element
-    const targetId = button.getAttribute("data-target");
+    // Add 'active' class to the clicked button
+    const activeCodeContainer = document.getElementById(button.dataset.target);
+    activeCodeContainer.style.display = "flex";
+    button.parentElement.classList.add("active");
+    button.parentElement.classList.add("text-purple");
 
-    const targetElement = document.getElementById(targetId);
+    button.parentElement.classList.remove("text-white/80");
+    button.parentElement.classList.remove("hover:text-white");
 
-    if (targetElement) {
-      targetElement.classList.remove("hidden");
-      targetElement.classList.add("flex");
-    }
-    document.querySelectorAll(".button-container").forEach((container) => {
-      container.classList.remove("bg-soft-purple");
-      container.classList.remove("text-purple");
-      container.classList.add("text-white/80");
-    });
+    // Move highlighter to the clicked button
+    const buttonRect = button.parentElement.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+    const translateX = buttonRect.x - containerRect.x;
 
-    const selectedContainer = button.closest(".button-container");
-
-    selectedContainer.classList.remove("text-white/80");
-    selectedContainer.classList.add("bg-soft-purple");
-    selectedContainer.classList.add("text-purple");
+    highlighter.style.transform = `translateX(${translateX}px)`;
   });
 });
