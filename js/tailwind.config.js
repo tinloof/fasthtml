75550526f8f20a83128491af5ed9a28274adeec0
Frederico Pena Santiago <frede.stg@gmail.com>
2024-07-07 14:48:16 -0300

    feat: Animate Samples section

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -2,6 +2,77 @@
 tailwind.config = {
   theme: {
     extend: {
+      keyframes: {
+        "shrink-and-grow": {
+          "0%": {
+            transform: "scale(0.7)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+        "positive-bounce": {
+          "0%": {
+            transform: "rotate(4deg)",
+          },
+          "33%": {
+            transform: "rotate(3deg)",
+          },
+          "66%": {
+            transform: "rotate(5deg)",
+          },
+          "100%": {
+            transform: "rotate(4deg)",
+          },
+        },
+        "negative-bounce": {
+          "0%": {
+            transform: "rotate(-4deg)",
+          },
+          "33%": {
+            transform: "rotate(-3deg)",
+          },
+          "66%": {
+            transform: "rotate(-5deg)",
+          },
+          "100%": {
+            transform: "rotate(-4deg)",
+          },
+        },
+        "line-bounce": {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "33%": {
+            transform: "scale(1.5)",
+          },
+          "66%": {
+            transform: "scale(.8)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+        "font-bounce-down": {
+          "0%": {
+            "font-size": "1rem",
+          },
+          "50%": {
+            "font-size": "0.98rem",
+          },
+
+          "100%": {
+            "font-size": "1rem",
+          },
+        },
+      },
+      animation: {
+        "positive-bounce": "positive-bounce  0.6s ease-in-out forwards",
+        "negative-bounce": "negative-bounce  0.6s ease-in-out forwards",
+        "line-bounce": "line-bounce  0.6s ease-in-out forwards",
+        "font-bounce-down": "font-bounce-down  0.6s ease-in-out forwards",
+        "shrink-and-grow": "shrink-and-grow 0.6s ease-in-out forwards",
+      },
       fontFamily: {
         geist: ["Geist", "sans-serif"],
         "geist-mono": ["Geist Mono", "monospace"],
