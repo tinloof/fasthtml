fcd8b040ba7f9552722a806c60ca0ce32543b03a
Jonathan Whitaker <johnowhitaker@gmail.com>
2024-07-24 14:59:31 -0700

    Make card demo work with touch events. Hijacks scroll but looks cool...

--- card3d.js
+++ card3d.js
@@ -1,16 +1,40 @@
 me().on('mouseenter', ev => {
     let e = me(ev)
     e.bounds = e.getBoundingClientRect()
-    e.on('mousemove', e.rotateToMouse)
+    e.on('mousemove', e.rotateToPointer)
 }).on('mouseleave', ev => {
     let e = me(ev)
-    e.off('mousemove', e.rotateToMouse)
+    e.off('mousemove', e.rotateToPointer)
     e.style.transform = e.style.background = ''
 })
-me().rotateToMouse = ev => {
-    let e = me(ev), b = e.bounds
-    let x = ev.clientX - b.x - b.width / 2
-    let y = ev.clientY - b.y - b.height / 2
+
+// Use a separate function for touch events to allow passive listener
+function handleTouchStart(ev) {
+    let e = me(ev)
+    e.bounds = e.getBoundingClientRect()
+    e.addEventListener('touchmove', e.rotateToPointer, { passive: true })
+    e.addEventListener('touchend', handleTouchEnd, { passive: true })
+}
+
+function handleTouchEnd(ev) {
+    let e = me(ev.target)
+    e.removeEventListener('touchmove', e.rotateToPointer)
+    e.removeEventListener('touchend', handleTouchEnd)
+    e.style.transform = e.style.background = ''
+}
+
+me().addEventListener('touchstart', handleTouchStart, { passive: true })
+
+me().rotateToPointer = ev => {
+    let e = me(ev.target), b = e.bounds
+    let x, y
+    if (ev.type === 'touchmove') {
+        x = ev.touches[0].clientX - b.x - b.width / 2
+        y = ev.touches[0].clientY - b.y - b.height / 2
+    } else {
+        x = ev.clientX - b.x - b.width / 2
+        y = ev.clientY - b.y - b.height / 2
+    }
     let d = Math.hypot(x,y)
     let amt = {amt}
     e.style.transform = `scale3d(${ 1 + 0.07 * amt }, ${ 1 + 0.07 * amt }, 1.0)
